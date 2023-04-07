718425d267799ac72fdcc6a285e4f1941632d44c
nikhilmkda <nikhilk9567@gmail.com>
2023-04-07 11:05:52 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:zedzat/provider.dart';
+
+import 'homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<DataProvider>(
+      create: (context) => DataProvider(),
+      child: MaterialApp(
+        theme: ThemeData(primaryColor: Colors.black),
+        debugShowCheckedModeBanner: false,
+        home: Homepage(),
+      ),
+    );
+  }
+}
